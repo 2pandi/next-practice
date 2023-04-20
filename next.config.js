33d415a3fdc1ba33641296fbce33d45943f0af20
yebin <yb.lee@elwide.com>
2023-04-20 20:21:32 +0900

    이미지 넣기(최적화)

--- next.config.js
+++ next.config.js
@@ -8,6 +8,17 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  // 외부 url로 Image 출력하려면 아래와 같은 설정을 해줘야 함
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3.amazonaws.com",
+        port: "",
+        pathname: "/my-bucket/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
